1852e99d66775ac5550128013f83ec5facaa363e
ZaVa94 <zvargas94@yahoo.com>
2016-10-10 12:33:27 -0600

    Create Levels.swift
    Added Levels.swift from Xcode.

---
+++ Levels.swift
@@ -0,0 +1,37 @@
+//
+//  Levels.swift
+//  MyFirstGame
+//
+//  Created by Zachary Vargas on 4/8/16.
+//  Copyright © 2016 Zach Vargas. All rights reserved.
+//
+
+class Levels {
+    var data = [
+        [
+            "Scene" : "1",
+            "NPC" : "Mom",
+            "Text" : "'The semester is almost over, finish strong!!' (Complete 5 days to finish the game)"
+        ],
+        [
+            "Scene" : "2",
+            "NPC" : "Girl",
+            "Text" : "'Hey! We only have a few days left in college, lets go live it up!' (Touch the text to go party)"
+        ],
+        [
+            "Scene" : "3",
+            "NPC" : "Professor",
+            "Text" : "'Almost time for finals! Lets get to it.' (Touch the text to watch the lecture)"
+        ],
+        [
+            "Scene" : "4",
+            "NPC" : "Mom",
+            "Text" : "'Too bad you didn't go to school sweetie, you might have been able to stop the nuclear apocalypse!'"
+        ],
+        [
+            "Scene" : "5",
+            "NPC" : "Mom",
+            "Text" : "Congratulations on finishing school and finding a job sweetie! I love the new house!"
+        ]
+    ]
+}
